0a311861f52b7360f21b599ae1838a1bc457d0e8
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2025-10-30 02:25:48 +0530

    Refactored "TreeDiagram" component and added focus effect on matching search node

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
-import ReactFlow, { Background, Controls } from "reactflow";
-import "reactflow/dist/style.css";
-import { generateTreeData, TREE_CONFIG } from "./utils";
+import TreeDiagram, { TreeDiagramProvider } from "./TreeDiagram";
 
 const json = {
   user: {
@@ -25,43 +22,11 @@ const json = {
 };
 
 export default function JsonTreeVisualizer() {
-  const [search, setSearch] = useState("");
-  const { nodes, edges } = useMemo(() => {
-    const { nodes, edges } = generateTreeData({ json });
-
-    const matchingNodeIndex = nodes.findIndex(
-      (node) => node.data.path === search
-    );
-
-    if (matchingNodeIndex !== -1) {
-      nodes[matchingNodeIndex].style = {
-        ...nodes[matchingNodeIndex].style,
-        ...TREE_CONFIG.selected,
-      };
-    }
-
-    return { nodes, edges };
-  }, [search]);
-
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <input
-        style={{
-          position: "fixed",
-          top: "4vh",
-          left: "50vw",
-          transform: "traslateX(-50%)",
-          zIndex: "999999",
-        }}
-        value={search}
-        onChange={(e) => {
-          setSearch(e.target.value);
-        }}
-      />
-      <ReactFlow nodes={nodes} edges={edges} fitView>
-        <Controls />
-        <Background />
-      </ReactFlow>
+      <TreeDiagramProvider>
+        <TreeDiagram json={json} />
+      </TreeDiagramProvider>
     </div>
   );
 }
---
+++ src/TreeDiagram.jsx
@@ -0,0 +1,62 @@
+import { useMemo, useState } from "react";
+import ReactFlow, {
+  Background,
+  Controls,
+  ReactFlowProvider,
+  useReactFlow,
+} from "reactflow";
+
+import { generateTreeData, TREE_CONFIG } from "./utils";
+
+import "reactflow/dist/style.css";
+
+export const TreeDiagramProvider = ReactFlowProvider;
+
+export default function TreeDiagram({ json }) {
+  const { fitView } = useReactFlow();
+
+  const [search, setSearch] = useState("");
+  const { nodes, edges } = useMemo(() => {
+    const { nodes, edges } = generateTreeData({ json });
+
+    const matchingNodeIndex = nodes.findIndex(
+      (node) => node.data.path === search
+    );
+
+    if (matchingNodeIndex !== -1) {
+      nodes[matchingNodeIndex].style = {
+        ...nodes[matchingNodeIndex].style,
+        ...TREE_CONFIG.selected,
+      };
+      fitView({ nodes: [{ id: nodes[matchingNodeIndex].id }], duration: 500 });
+    }
+
+    return { nodes, edges };
+  }, [search, json, fitView]);
+
+  return (
+    <>
+      <input
+        style={{
+          position: "fixed",
+          top: "4vh",
+          left: "50vw",
+          transform: "traslateX(-50%)",
+          zIndex: "999999",
+          borderRadius: "0.5rem",
+          border: "none",
+          color: "#fff",
+          fontSize: "1.5rem",
+        }}
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <ReactFlow nodes={nodes} edges={edges} fitView>
+        <Controls />
+        <Background />
+      </ReactFlow>
+    </>
+  );
+}
